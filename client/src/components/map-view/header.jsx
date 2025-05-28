954e1bf0476584f582cea7f361d1134438be718f
Bipin <bipinmaharjan941@gmail.com>
2025-05-28 20:17:21 +0545

    Header pc view ma hamburger menu haldeko

--- client/src/components/map-view/header.jsx
+++ client/src/components/map-view/header.jsx
@@ -26,7 +26,7 @@ function HeaderRightContent() {
     <div className="flex lg:items-center lg:flex-row flex-col gap-4">
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Avatar className="bg-black">
+          <Avatar className="bg-black cursor-pointer"> {/* Added cursor-pointer */}
             <AvatarFallback className="bg-[#E6E0D3] text-[#070f18] font-extrabold">
               {user?.userName[0].toUpperCase()}
             </AvatarFallback>
@@ -56,7 +56,7 @@ export function MappingHeader({ setOpen }) {
   return (
     <header className="bg-[#070f18] sticky top-0 z-40 w-full border-b border-[#070f18] pt-3 ">
       <div className="flex h-1/6 text-xl items-center justify-between px-4 sm:px-6 pt-5">
-        <Button onClick={() => setOpen(true)} className="lg:hidden sm:block">
+        <Button onClick={() => setOpen(true)} className="sm:block">
           <AlignJustify />
           <span className="sr-only">Toggle Menu</span>
         </Button>
