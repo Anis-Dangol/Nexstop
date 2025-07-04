b4321e3e1d98a85cf64a155afff098ef270d3f45
Rohan <rohanmaharjan181x@gmail.com>
2025-07-04 08:36:50 +0545

    add

--- client/src/pages/map-home/MappingHome.jsx
+++ client/src/pages/map-home/MappingHome.jsx
@@ -2,11 +2,12 @@ import MapContainerWrapper from "@/components/map-view/MapContainerWrapper";
 
 function MappingHome() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="relative w-full h-screen overflow-hidden">
-        <MapContainerWrapper />
-      </div>
-    </div>
+    <div></div>
+    // <div className="flex flex-col min-h-screen">
+    //   <div className="relative w-full h-screen overflow-hidden">
+    //     {/* <MapContainerWrapper /> */}
+    //   </div>
+    // </div>
   );
 }
 
